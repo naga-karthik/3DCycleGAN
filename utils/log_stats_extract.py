e5f98b4d606a90d3ba04fc6b3c197cfa9e26a2cb
naga-karthik <emvnagakarthik@gmail.com>
2020-06-21 16:21:55 -0400

    routine update for new data

--- utils/log_stats_extract.py
+++ utils/log_stats_extract.py
@@ -2,12 +2,13 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 
-path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/saved_models/mr2ct_new_unetUpdated/'
+# path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/saved_models/mr2ct_new_unetUpdated/'
 # path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/saved_models/mr2ct_unet_batchsize16/'
 # path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/saved_models/mr2ct_spectral/'
+path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/utils/'
 
 lines = []
-with open(path+'log_stats.txt', 'rt') as file:
+with open(path+'log_stats_v1.txt', 'rt') as file:
     for line in file:
         lines.append(line)
 
@@ -15,33 +16,50 @@ n = len(lines)
 G_loss = []   # every 8th element
 G_adversarial_loss = [] # every 13th element
 G_cycle_loss = [] # every 18th element
-G_identity = []  # every 23rd element
+G_gradient_loss = []  # every 23rd element
 D_loss = []  # every 27th element
 epochs = np.arange(1, n+1)
 
+# for idx in range(len(lines)):
+#     temp = [str for str in lines[idx].split()]
+#     if idx < 100: # 99: # 89:
+#         G_loss.append(float(temp[10+1]))
+#         G_adversarial_loss.append(float(temp[15+1]))
+#         G_cycle_loss.append(float(temp[20+1]))
+#         G_identity.append(float(temp[25]))
+#         D_loss.append(float(temp[29]))
+#     else:
+#         G_loss.append(float(temp[8]))
+#         G_adversarial_loss.append(float(temp[13]))
+#         G_cycle_loss.append(float(temp[18]))
+#         G_identity.append(float(temp[23]))
+#         D_loss.append(float(temp[27]))
+
 for idx in range(len(lines)):
     temp = [str for str in lines[idx].split()]
-    if idx < 54: # 99: # 89:
-        G_loss.append(float(temp[10]))
-        G_adversarial_loss.append(float(temp[15]))
-        G_cycle_loss.append(float(temp[20]))
-        G_identity.append(float(temp[25]))
+    if idx < 164: # 99: # 89:
+        G_adversarial_loss.append(float(temp[11]))
+        G_cycle_loss.append(float(temp[16]))
+        G_gradient_loss.append(float(temp[21]))
+        G_loss.append(float(temp[25]))
         D_loss.append(float(temp[29]))
     else:
-        G_loss.append(float(temp[8]))
-        G_adversarial_loss.append(float(temp[13]))
-        G_cycle_loss.append(float(temp[18]))
-        G_identity.append(float(temp[23]))
+        print(idx)
+        G_adversarial_loss.append(float(temp[9]))
+        G_cycle_loss.append(float(temp[14]))
+        G_gradient_loss.append(float(temp[19]))
+        G_loss.append(float(temp[23]))
         D_loss.append(float(temp[27]))
+
 
 
 # print(G_loss)
 # print(G_cycle_loss)
 
-plt.plot(epochs, G_loss, 'bo-', label='Generator Loss', markersize=4, markevery=4)
-plt.plot(epochs, G_cycle_loss, 'g+-', label='Cycle Consistency Loss', markersize=4, markevery=4)
-plt.plot(epochs, G_identity, 'rx-', label='Identity Loss', markersize=4, markevery=4)
 plt.plot(epochs, G_adversarial_loss, 'c1-', label='Generator Adversarial Loss', markersize=4, markevery=4)
+plt.plot(epochs, G_cycle_loss, 'g+-', label='Cycle Consistency Loss', markersize=4, markevery=4)
+plt.plot(epochs, G_gradient_loss, 'rx-', label='Gradient Consistency Loss', markersize=4, markevery=4)
+plt.plot(epochs, G_loss, 'bo-', label='Generator Loss', markersize=4, markevery=4)
 plt.plot(epochs, D_loss, 'ys-', label='Discriminator Loss', markersize=4, markevery=4)
 plt.xlabel('Epochs')
 plt.ylabel('Loss Values')
--- utils/numpyToNrrd.py
+++ utils/numpyToNrrd.py
@@ -7,73 +7,78 @@ import matplotlib.pyplot as plt
 # # load_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/saved_images/training_generated/' \
 # #             'mr2ct_new_unetUpdated/realA2fakeB/'
 # load_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/saved_images/training_generated/' \
-#             'mr2ct_v1_lighterUnet_gradientConsistency_batchSize4/realA2fakeB/'
+#             'mr2ct_v3_lighterUnet_gradientConsistency_batchSize4/realA2fakeB/0001.npy'
 # # load_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/saved_images/training_generated/' \
 # #             'mr2ct_spectral/realA2fakeB/'
-# # load_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/saved_images/testing_generated/' \
-# #             'mr2ct_spectral/realA2fakeB/diverse_epoch_200/0001.npy'
-# # files = sorted(glob.glob(load_path))
-# path = load_path+'*.npy'
-# files = sorted(glob.glob(path))
+# # load_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/datasets/mr2ct_new/test/A/normalized_patient11_23.npy'
+# files = sorted(glob.glob(load_path))
+# # path = load_path+'*.npy'
+# # files = sorted(glob.glob(path))
 #
 # # save_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/nrrd_files/mr2ct_new_unetUpdated'
 # # save_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/nrrd_files/mr2ct_unet'
 # save_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/nrrd_files/' \
-#             'mr2ct_v1_lighterUnet_gradientConsistency_batchSize4'
-# # save_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/nrrd_files'
+#             'mr2ct_v3_lighterUnet_gradientConsistency_batchSize4'
+# # save_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/datasets/mr2ct_new/test/A1'
 # if not os.path.exists(save_path):
 #     os.mkdir(save_path)
 # file_name = files[0]
 # f = file_name.split('/')
 # # print(f[-1][:-4])
 # mri_data = np.load(file_name).squeeze(0)    # loaded as: 48x256x128
-# # print(mri_data.shape, mri_data.dtype, mri_data.min(), mri_data.max())
+# print(mri_data.shape, mri_data.dtype, mri_data.min(), mri_data.max())
 # # sys.exit()
 # mri_data_1 = np.transpose(mri_data, (0, 2, 1))
+#
 # # mri_data_1 = np.transpose(mri_data, (2, 1, 0))
+# # mri_data_1 = np.flip(mri_data) #, axis=1)
+# # print(mri_data_1.shape)
+# # np.save(save_path+'/' + f[-1][:-4], mri_data_1)
+#
 # # bringing it in 0-255 range
 # # mri_data_1 = (255*((mri_data_1+1)/2.0)).astype(np.uint8)   # first bringing it into 0-1 range and multipyling my 255
+#
 # nrrd.write(save_path+'/train_' + f[-1][:-4] + '.nrrd', mri_data_1)
-# # nrrd.write(save_path+'/divtest_0001_uint.nrrd', mri_data_1)
+# # nrrd.write(save_path+'/' + f[-1][:-4] + '.nrrd', mri_data_1)
 
 
-# --------------- for testing .npy files ----------------
-epoch_nums = [30, 80, 120, 180, 250]
-# epoch_nums = [70, 80, 90, 100]
-epch = epoch_nums[0]
-# load_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/saved_images/testing_generated/mr2ct_spectral' \
-#             '/realA2fakeB/epoch_' + str(epch) + '/'
+# # --------------- for testing .npy files ----------------
+# epoch_nums = [60, 80, 120, 160, 180]
+# # epoch_nums = [70, 80, 90, 100]
+# epch = epoch_nums[4]
+# # load_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/saved_images/testing_generated/mr2ct_spectral' \
+# #             '/realA2fakeB/epoch_' + str(epch) + '/'
+# # load_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/saved_images/testing_generated/' \
+# #             'mr2ct_unet_gradcon_exp1/realA2fakeB/epoch_' + str(epch) + '/'
 # load_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/saved_images/testing_generated/' \
-#             'mr2ct_unet_gradcon_exp1/realA2fakeB/epoch_' + str(epch) + '/'
-load_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/saved_images/testing_generated/' \
-            'mr2ct_v1_lighterUnet_gradientConsistency_batchSize4/realA2fakeB/epoch_' + str(epch) + '/'
-path = load_path + '*.npy'
-files = sorted(glob.glob(path))
-
+#             'mr2ct_v1_lighterUnet_gradientConsistency_batchSize4/realA2fakeB/epoch_' + str(epch) + '/'
+# path = load_path + '*.npy'
+# files = sorted(glob.glob(path))
+#
+# # save_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/nrrd_files/' \
+# #             'mr2ct_unet_gradient_consistency/diverse_epoch_' + str(epch)
+# # save_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/nrrd_files/mr2ct_unet/diverse_epoch_'+str(epch)
 # save_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/nrrd_files/' \
-#             'mr2ct_unet_gradient_consistency/diverse_epoch_' + str(epch)
-# save_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/nrrd_files/mr2ct_unet/diverse_epoch_'+str(epch)
-save_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/nrrd_files/' \
-            'mr2ct_v1_lighterUnet_gradientConsistency_batchSize4/epoch_' +str(epch)
-# save_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/nrrd_files/new_test_data/'
-if not os.path.exists(save_path):
-    os.mkdir(save_path)
-
-# print(np.load(files[0]).shape)
-# sys.exit()
-for file_name in files:
-    temp = file_name.split('/')
-    name = temp[-1][:-4]
-    mri_data = np.load(file_name).squeeze(0)  # loaded as: 48x256x128
-    mri_data_1 = np.transpose(mri_data, (0, 2, 1))
-    # mri_data = np.load(file_name)  # loaded as: 256x128x48
-    # mri_data_1 = np.transpose(mri_data, (2, 1, 0))
-
-    # bringing it in 0-255 range
-    # mri_data_1 = (255*((mri_data_1+1)/2.0)).astype(np.uint8)  # first bringing it into 0-1 range and multiply by 255
-
-    # nrrd.write(save_path+'/gradcon_exp1_new_data_' + name + '.nrrd', mri_data_1)
-    nrrd.write(save_path + '/test_ep30_' + name + '.nrrd', mri_data_1)
+#             'mr2ct_v1_lighterUnet_gradientConsistency_batchSize4/epoch_' +str(epch)
+# # save_path = '/home/karthik/PycharmProjects/DLwithPyTorch/cycleGAN3D/nrrd_files/new_test_data/'
+# if not os.path.exists(save_path):
+#     os.mkdir(save_path)
+#
+# # print(np.load(files[0]).shape)
+# # sys.exit()
+# for file_name in files:
+#     temp = file_name.split('/')
+#     name = temp[-1][:-4]
+#     mri_data = np.load(file_name).squeeze(0)  # loaded as: 48x256x128
+#     mri_data_1 = np.transpose(mri_data, (0, 2, 1))  # because Slicer needs it as 48x128x256
+#     # mri_data = np.load(file_name)  # loaded as: 256x128x48
+#     # mri_data_1 = np.transpose(mri_data, (2, 1, 0))
+#
+#     # bringing it in 0-255 range
+#     # mri_data_1 = (255*((mri_data_1+1)/2.0)).astype(np.uint8)  # first bringing it into 0-1 range and multiply by 255
+#
+#     # nrrd.write(save_path+'/gradcon_exp1_new_data_' + name + '.nrrd', mri_data_1)
+#     nrrd.write(save_path + '/test_ep180_' + name + '.nrrd', mri_data_1)
 
 # -------------------------------------------------------------------------
 # shape of the original 3D matrix MRI - (217, 123, 104)
